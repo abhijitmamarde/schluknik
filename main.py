9e7eb68020a8800eb79da35f19b6383f90800e85
Elias <elias.kardel@posteo.de>
2016-06-02 13:56:00 +0200

    First file transfer to server

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from ftplib import FTP
 
 red = [1,0,0,1]
 green = [0,1,0,1]
@@ -43,17 +44,26 @@ class VBoxLayoutExample(App):
         """"""
         self.orient = orient
 
-
+	
     def callback(instance, event):
-	print(event.text)
-    	print('Some button is being pressed')  
+	#domain name or server ip:
+	ftp = FTP('singing-wires.de') 
+	ftp.login(user='web784', passwd = 'Holz0815')
+	ftp.cwd('/html/schluknik')
+	filename = event.text + ".txt"
+	with open(filename,"a+") as f:		
+		ftp.storlines("STOR " + filename, open(filename, 'r'))
+		ftp.quit()
+		print('sucessfully created file'+ filename)
+	
+
     #----------------------------------------------------------------------
     def build(self):
         """"""
         layout = BoxLayout(padding=10, orientation=self.orient)
 
         for i in range(5):
-            btn = Button(text="Button %s" % (i+1) )
+            btn = Button(text="Button%s" % (i+1) )
 	    btn.bind(on_press=self.callback)
             layout.add_widget(btn)
         return layout
