44712b7abdfab69b2698882c9454acb53095d030
Elias <elias.kardel@posteo.de>
2016-06-02 10:23:27 +0200

    Initial commit from virtual box

--- main.py
+++ main.py
@@ -1,10 +1,66 @@
+import kivy
+import random
+
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.base import EventLoop 
-  
-class Hello(App):
+from kivy.uix.boxlayout import BoxLayout
+
+red = [1,0,0,1]
+green = [0,1,0,1]
+blue =  [0,0,1,1]
+purple = [1,0,1,1]
+
+########################################################################
+class HBoxLayoutExample(App):
+    """
+    Horizontally oriented BoxLayout example class
+    """
+
+    #----------------------------------------------------------------------
     def build(self):
-        btn = Button(text='Hello World 2', color=(1, 1, 0, 1), background_color=(255, 255, 255, 255))
-        return  btn
-  
-Hello().run()
+        """
+        Horizontal BoxLayout example
+        """
+        layout = BoxLayout(padding=10)
+        colors = [red, green, blue, purple]
+
+        for i in range(5):
+            btn = Button(text="Button #%s" % (i+1),
+                         background_color=random.choice(colors)
+                         )
+
+            layout.add_widget(btn)
+        return layout
+
+########################################################################
+class VBoxLayoutExample(App):
+    """
+    Vertical oriented BoxLayout example class
+    """
+
+    #----------------------------------------------------------------------
+    def setOrientation(self, orient):
+        """"""
+        self.orient = orient
+
+
+    def callback(instance, event):
+	print(event.text)
+    	print('Some button is being pressed')  
+    #----------------------------------------------------------------------
+    def build(self):
+        """"""
+        layout = BoxLayout(padding=10, orientation=self.orient)
+
+        for i in range(5):
+            btn = Button(text="Button %s" % (i+1) )
+	    btn.bind(on_press=self.callback)
+            layout.add_widget(btn)
+        return layout
+
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+    #app = HBoxLayoutExample()
+    app = VBoxLayoutExample()
+    app.setOrientation(orient="vertical")
+    app.run()
