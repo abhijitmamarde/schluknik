72a4fdbe289b089343c838df97f80e1d2393f5f6
Elias.Kardel <elias.kardel@u-blox.com>
2016-06-20 14:02:18 +0200

    clean up code

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-﻿########################################################################
-#			schluknik 2016
-#			run with python 2.7.6
-########################################################################
+﻿#!/usr/bin/env python
+"""
+		schluknik 2016
+		run with python 2.7.6
+"""
+
+####    Author Information      ####
+
+__author__ = "Elias Kardel"
+__copyright__ = "Copyright 2016t"
+__credits__ = ["Elias Kardel"]
+__license__ = "GPL"
+__maintainer__ = "Elias Kardel"
+__email__ = "elias.kardel@u-blox.com"
+__status__ = "development"
+
+###     Imports     ###
+
 import kivy
 kivy.require('1.0.7')
 
@@ -14,33 +28,44 @@ from ftplib import FTP
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.core.image import Image
-
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
 from kivy.core.window import Window
-########################################################################
+
+###     Body        ###
 class VBoxLayoutExample(App):
-    #----------------------------------------------------------------------
+    """
+    Main class
+    """
     def setOrientation(self, orient):
-		self.orient = orient
-    #--- Callback when button is pressed
+        """
+		Todo
+        """
+        self.orient = orient
+
     def callback(instance, event):
-		#--- File FTP transfer
-		#domain name or server ip:
-		ftp = FTP('singing-wires.de')
+        """
+		Button pressed handler
+        """
+        # File FTP transfer
+		# domain name or server ip:
+        ftp = FTP('singing-wires.de')
 		# how to encrypt this?
-		ftp.login(user='web784', passwd = 'Holz0815')
-		ftp.cwd('/html/schluknik')
-		filename = event.text + ".txt"
+        ftp.login(user='web784', passwd = 'Holz0815')
+        ftp.cwd('/html/schluknik')
+        filename = event.text + ".txt"
 		# todo: store as .csv file
-		with open(filename,"a+") as f:		
-			ftp.storlines("STOR " + filename, open(filename, 'r'))
-			ftp.quit()
-			print('sucessfully created file'+ filename)
-    #--- Introductional screen
+        with open(filename,"a+") as f:		
+            ftp.storlines("STOR " + filename, open(filename, 'r'))
+            ftp.quit()
+            print('sucessfully created file'+ filename)
+    
     def build(self):
+        """
+		Layout builder of main window
+        """
         self.icon = 'icon.png'
         Window.clearcolor = (1, 1, 1, 0)	
         layout = BoxLayout(padding=10, orientation=self.orient)
@@ -55,8 +80,8 @@ class VBoxLayoutExample(App):
 			btn.bind(on_press=self.callback)
 			layout.add_widget(btn)
         return layout
-########################################################################
 
+###     Class section       ###
 class RootScreen(ScreenManager):
     pass
 class StartScreen(FloatLayout):
@@ -69,9 +94,11 @@ class TestApp(App):
 		
 		return StartScreen()
 
-#--- Entry Point
+#######
 if __name__ == "__main__":
+    """
+		Entry point for the application
+    """
     app = VBoxLayoutExample()
     app.setOrientation(orient="vertical")  
-    #TestApp().run()
     app.run()
